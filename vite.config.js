df0c55cbe1d0234418051d4029367e5de186cd69
Siddu2000 <siddeshs044@gmail.com>
2024-03-04 10:47:08 +0530

    staging access removed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       "/graphql":{
         target:"https://www.tiger-one.eu/",
+        // target:"https://staging.tiger-one.eu/",
+        // target:"https://staging.seedsman.com/",
         changeOrigin:true
       }
     }
